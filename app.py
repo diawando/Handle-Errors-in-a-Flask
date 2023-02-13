81670c3904e13e43e87f3c2a42f8eecdf8de5d1b
diawando <abduldiawade777@gmail.com>
2023-02-13 18:32:03 +0000

    creation du fichier 500.html et ecriture de sa structure.html

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def page_not_found(error):
     return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -17,3 +21,7 @@ def message(idx):
         return render_template('message.html', message=messages[idx])
     except IndexError:
         abort(404)
+        
+@app.route('/500')
+def error500():
+    abort(500)
