0006f885c82b9d5e8b6c277f6c60341d42dbec5d
diawando <abduldiawade777@gmail.com>
2023-02-13 18:18:12 +0000

    creation d'un errorhandler qui s'affiche dans un fichier 404.html, ajout du fichier 404.html et ecriture de sa structure html

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 @app.route('/')
 def index():
     return render_template('index.html')
