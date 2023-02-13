0f0657212f6328527149f8d8653fcea113963257
diawando <abduldiawade777@gmail.com>
2023-02-13 17:40:55 +0000

    ajout de la route message pour la personnalisation des messages d'erreurs

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/messages/<int:idx>')
+def message(idx):
+    messages = ['Message Zero', 'Message One', 'Message Two']
+    return render_template('message.html', message=messages[idx])
